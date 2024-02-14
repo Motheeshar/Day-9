ae3eeb24b9f75255227784782a6e8d9f37c9c5be
A R Motheesh <ar.motheesh@gmail.com>
2024-02-14 15:31:00 +0530

    Add files via upload

---
+++ Assignment on JS Operators.js
@@ -0,0 +1,88 @@
+var x = 5;
+var y = 2;
+
+// Challenge 1
+console.log("Challenge 1:", x + y);
+document.getElementById("c-1").innerHTML = "Challange 1: " + x + y;
+
+// Challenge 2
+console.log("Challenge 2:", x - y);
+document.getElementById("c-2").innerHTML = "Challange 2: " + (x - y);
+
+// Challenge 3
+console.log("Challenge 3:", x * y);
+document.getElementById("c-3").innerHTML = "Challange 3: " + x * y;
+
+// Challenge 4
+console.log("Challenge 4:", x / y);
+document.getElementById("c-4").innerHTML = "Challange 4: " + x / y;
+
+// Challenge 5
+console.log("Challenge 5:", x % y);
+document.getElementById("c-5").innerHTML = "Challange 5: " + (x % y);
+
+// Challenge 6
+console.log("Challenge 6:", Math.pow(x, y));
+document.getElementById("c-6").innerHTML = "Challange 6: " + Math.pow(x, y);
+
+var z = 20;
+
+// Challenge 7
+console.log("Challenge 7:", Math.pow(x, y));
+document.getElementById("c-7").innerHTML = "Challange 7: " + Math.pow(x, y);
+
+// Challenge 8
+z--;
+console.log("Challenge 8:", z);
+document.getElementById("c-8").innerHTML = "Challange 8: " + z;
+
+var a = 20;
+var b = 10;
+var c = 15;
+var d = 20;
+
+// Challenge 9
+a += x;
+console.log("Challenge 9:", a);
+document.getElementById("c-9").innerHTML = "Challange 9: " + a;
+
+// Challenge 10
+b -= y;
+console.log("Challenge 10:", b);
+document.getElementById("c-10").innerHTML = "Challange 10: " + b;
+
+// Challenge 11
+c *= x;
+console.log("Challenge 11:", c);
+document.getElementById("c-11").innerHTML = "Challange 11: " + c;
+
+// Challenge 12
+d /= x;
+console.log("Challenge 12:", d);
+document.getElementById("c-12").innerHTML = "Challange 12: " + d;
+
+// Challenge 13
+console.log("Challenge 13:", x === y, x === 5);
+document.getElementById("c-13").innerHTML =
+  "Challange 13: " + (x === y, x === 5);
+
+// Challenge 14
+console.log("Challenge 14:", x !== y, x !== 5);
+document.getElementById("c-14").innerHTML =
+  "Challange 14: " + (x !== y, x !== 5);
+
+// Challenge 15
+console.log("Challenge 15:", x < y, x < 10);
+document.getElementById("c-15").innerHTML = "Challange 15: " + (x < y, x < 10);
+
+// Challenge 16
+console.log("Challenge 16:", x > y, x > 10);
+document.getElementById("c-16").innerHTML = "Challange 16: " + (x > y, x > 10);
+
+// Challenge 17
+console.log("Challenge 17:", x <= y, x <= 5);
+document.getElementById("c-17").innerHTML = "Challange 17: " + (x <= y, x <= 5);
+
+// Challenge 18
+console.log("Challenge 18:", x >= y, x >= 5);
+document.getElementById("c-18").innerHTML = "Challange 18: " + (x >= y, x >= 5);
